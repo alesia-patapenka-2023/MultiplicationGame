e3513d5b9195cf3f2f7c1eb1bfed5f96d6249b12
alesia <alesiniakv@gmail.com>
2023-02-13 22:24:07 +0300

    added modified file

--- Problem1MultiplicationforKids.py
+++ Problem1MultiplicationforKids.py
@@ -5,26 +5,35 @@ seed(100)
 if __name__=="__main__":
     count = 0
     test_passed = 7
+    rezume = []
+    example = ""
     for i in range(10):
         first = randint(1, 10)
         second = randint(1, 10)
         result = first * second
-        print(f"Question: {first} x {second} = ?", end=" ")
+        example = f"{first} x {second} = ?"
+        print(f"Question: {example}", end= "")
         try:
             answer = int(input())
         except ValueError:
             answer = None
-            print("No")
-
         if (answer == result):
-            print("yes")
+            print("Right")
+            correct_answer = f"Q{i+1}: {example}; Student Answer: {answer}; Answer: {result}; Status: CORRECT"
+            rezume.append(correct_answer)
             count = count + 1
         else:
-            print("no")
-    print(f"Right is {count}")
+            print(f"Wrong! The right answer is: {result}")
+            wrong_answer = f"Q{i+1}: {example}; Student Answer: {answer}; Answer: {result}; Status: WRONG"
+            rezume.append(wrong_answer)
+    print(f"Number of correct answers is: {count}")
     if (count >= test_passed):
         print("Congratulations!!!")
     else:
         print("try again!")
+    print("*** SUMMARY ***")
+    for i in range(10):
+        print(rezume[i])
+
 
 
